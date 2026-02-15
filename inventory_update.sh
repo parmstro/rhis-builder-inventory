6e4dbf7610c61e020037ac821ed4b23785638f4e
heatmiser <mike.savage@gmail.com>
2026-02-15 11:49:49 -0500

    Pass present working directory external to container runtime as variable within container.

--- inventory_update.sh
+++ inventory_update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Executing inventory update."
-podman run -it -v $PWD:/rhis/external_inventory:Z \
-                --workdir /rhis/external_inventory \
-                --hostname inventory_update \
-                quay.io/s4v0/centosstream10-ansible:latest ansible-playbook -u ansiblerunner inventory_update.yml
+podman run -it --env working_directory_external_to_container="$PWD" \
+               --volume $PWD:/rhis/external_inventory:Z \
+               --workdir /rhis/external_inventory \
+               --hostname inventory_update \
+               quay.io/s4v0/centosstream10-ansible:latest ansible-playbook -u ansiblerunner inventory_update.yml --extra-vars "working_directory_external_to_container=${working_directory_external_to_container}"
