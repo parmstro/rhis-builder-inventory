3d08b7989185653c3127f181af80f6810557c153
Mike Savage <mike.savage@gmail.com>
2026-02-25 21:05:11 -0500

    Update to use userns=keep-id to match user uid inside and outside container.

--- inventory_update.sh
+++ inventory_update.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Executing inventory update."
-podman run -it --env working_directory_external_to_container="$PWD" \
-               --volume $PWD:/rhis/external_inventory:Z \
-               --workdir /rhis/external_inventory \
-               --hostname inventory_update \
-               quay.io/s4v0/centosstream10-ansible:latest ansible-playbook -u ansiblerunner inventory_update.yml --extra-vars "working_directory_external_to_container=${working_directory_external_to_container}"
+podman run --rm --userns=keep-id --env working_directory_external_to_container="$PWD" \
+           --volume $PWD:/rhis/external_inventory:Z \
+           --workdir /rhis/external_inventory \
+           --hostname inventory_update \
+           quay.io/s4v0/centosstream10-ansible:latest ansible-playbook inventory_update.yml \
+           --extra-vars "working_directory_external_to_container=${working_directory_external_to_container}"
