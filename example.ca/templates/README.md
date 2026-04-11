94f85f0c7c784bc71f6a00b35e8ce787e3f85f69
parmstro <parmstro@redhat.com>
2026-04-11 01:04:01 +0000

    delete the sample files, they are now generated

--- example.ca/templates/README.md
+++
@@ -1,3 +0,0 @@
-## Templates Directory
-This folder contains templates consumed by RHIS configurations
-e.g. Open SCAP files
--- example.ca/templates/testruby.rb
+++
@@ -1,5 +0,0 @@
-$discovered_drives = `blkid -t TYPE=crypto_LUKS -o device`.split(/$/).map(&:strip)
-$discovered_drives.pop
-$discovered_drives.each do |drv|
-  puts drv
-end
