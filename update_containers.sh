8889f59f7825ae39f8f43f2b770c5058af0f8ff6
parmstro <parmstro@redhat.com>
2026-08-13 15:07:56 +0000

    deprecate the AAP 2.4 container scripts

--- update_containers.sh
+++ update_containers.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-podman pull quay.io/parmstro/rhis-provisioner-9-2.4:latest
-podman tag  quay.io/parmstro/rhis-provisioner-9-2.4:latest localhost/rhis-provisioner-9-2.4:latest
 podman pull quay.io/parmstro/rhis-provisioner-9-2.5:latest
 podman tag  quay.io/parmstro/rhis-provisioner-9-2.5:latest localhost/rhis-provisioner-9-2.5:latest
 podman pull quay.io/parmstro/rhis-builder-ui:latest
